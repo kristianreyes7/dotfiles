158a2761f93b6fe8bf1d1ef2a86aa418eebc8960
kristianreyes7 <kristianreyes7@icloud.com>
2023-04-20 16:05:51 -0700

    executable

---
+++ install_lg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
+tar xf lazygit.tar.gz -C /usr/local/bin lazygit
