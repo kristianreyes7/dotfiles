d8c84d0a8c90d99c8041ecf7c76315f6b61e124e
kristianreyes7 <kristianreyes7@icloud.com>
2023-04-21 09:12:53 -0700

    lazy git again

--- install_lg.sh
+++ install_lg.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
-
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-
-tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
